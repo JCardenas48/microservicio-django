f8306eef599721ac6d3dbb956723f1253d059e7e
Javier Cardenas <jreyescardenas42>
2021-11-14 02:03:50 -0500

    todo de nuevo

--- soporte/urls.py
+++ soporte/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from django.urls import path
-from django.urls.resolvers import URLPattern
 from .views import PQRListCreate, PQRUpdateDelete, PersonaSoporteListCreate, PersonaSoporteUpdateDelete
 
 URLPattern = {
