29bd820ca43b00442e353a5335db350d43aa1324
Javier Cardenas <jreyescardenas42>
2021-11-13 20:50:51 -0500

    new urls for CRUD in persona-soporte

---
+++ soporte/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.urls.resolvers import URLPattern
+from .views import PQRListCreate, PQRUpdateDelete, PersonaSoporteListCreate, PersonaSoporteUpdateDelete
+
+URLPattern = {
+    path('personas-soporte/', PersonaSoporteListCreate.as_view()),
+    path('personas-soporte/<pk>/', PersonaSoporteUpdateDelete.as_view()),  
+    path('pqr/', PQRListCreate.as_view()),
+    path('pqr/<pk>/', PQRUpdateDelete.as_view())
+}
