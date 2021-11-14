265e924d5c6dad75f3cd761d8f81cd759e21eb31
Javier Cardenas <jreyescardenas42>
2021-11-14 12:11:24 -0500

    fix urls

--- soporte/urls.py
+++ soporte/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import PQRListCreate, PQRUpdateDelete, PersonaSoporteListCreate, PersonaSoporteUpdateDelete
 
-URLPattern = {
+urlPattern = {
     path('personas-soporte/', PersonaSoporteListCreate.as_view()),
     path('personas-soporte/<pk>/', PersonaSoporteUpdateDelete.as_view()),  
     path('pqr/', PQRListCreate.as_view()),
