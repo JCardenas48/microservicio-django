7bee0f8ee3ac8b9fdd0317131ba5c180d91fb462
Javier Cardenas <jreyescardenas42>
2021-11-14 15:18:57 -0500

    add urls sub class

--- soporte/serializers.py
+++ soporte/serializers.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models import fields
 from rest_framework import serializers
+from rest_framework.fields import ReadOnlyField
 from .models import PQR, PersonaSoporte
 
 class PersonaSoporteSerializer(serializers.ModelSerializer):
@@ -9,6 +10,8 @@ class PersonaSoporteSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class PQRSerializer(serializers.ModelSerializer):
+    persona_soporte = PersonaSoporteSerializer(read_only=True)
+    
     class Meta:
         model = PQR
         fields = {'persona_soporte', 'estado', 'comentario'}
